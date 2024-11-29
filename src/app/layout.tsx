8bcba780995a5e862dec043d26e4b88dac0e6f56
Nhahan <kisy324@naver.com>
2024-11-29 13:50:05 +0900

    feat: metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css';
 import { ReactNode, Suspense } from 'react';
 import ThemeProvider from '@/provider/ThemeProvider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Crypto Chart Widget',
+  description: 'Crypto chart widget for your GitHub README',
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
